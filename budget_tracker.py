7614cb5d0c0f511b543628a33d6faadb54fd5da7
Faris <140075663+mzFaris@users.noreply.github.com>
2025-01-02 20:16:59 +0700

    connect to mongodb and add function
    add function income and expense

--- budget_tracker.py
+++ budget_tracker.py
@@ -1,3 +1,14 @@
+from pymongo import MongoClient
+import datetime
+
+
+client = MongoClient("mongodb://localhost:27017")
+db = client["financeApp"]
+incomes_collection = db["incomes"]
+expenses_collection = db["expenses"]
+categories_collection = db["categories"]
+
+
 def main():
     ...
     
